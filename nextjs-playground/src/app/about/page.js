09212870481d290f3c0e8174be414e188c719bde
Pavan Hutagi <pavanhutagi@gmail.com>
2023-05-19 21:13:19 +0530

    Initial folder structure

---
+++ nextjs-playground/src/app/about/page.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <>
+        <h1> I am About </h1>
+        <p>This is a About Page</p>
+      </>
+    )
+  }
+  
---
+++ nextjs-playground/src/app/contact/page.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+      <>
+        <h1> I am Contact </h1>
+        <p>This is a Contact Page</p>
+      </>
+    )
+  }
+  
